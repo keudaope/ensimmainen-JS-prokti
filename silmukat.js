d8a53640545e93706eaebdf1f0b8a649797415fb
Jyri Lindroos <jyrilindroos@Jyrin-MacBook.local>
2020-08-24 08:21:37 +0300

    ehtolauseita ja silmukoita

---
+++ silmukat.js
@@ -0,0 +1,23 @@
+for(var i = 0; i < 10; i++)
+{
+  console.log(i + '\n');
+}
+
+var i = 0;
+while(i < 10)
+{
+  console.log(i + '\n');
+  i++;
+}
+var i = 0;
+do
+{
+  console.log(i + '\n');
+  i++;
+}while(i < 10);
+
+var lemmikki = 'hevonen';
+for(var j = (lemmikki.length-1); j >= 0; j--)
+{
+  console.log(lemmikki[j]);
+}
